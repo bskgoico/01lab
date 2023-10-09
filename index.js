bac492e37413b933f6753836decf594794798fcc
fnkbsk <igoico10@gmail.com>
2023-10-09 18:24:58 -0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const nav = document.querySelector('.nav'),
-    navItem = nav.querySelector('.nav_list_item');
+    navItem = nav.querySelectorAll('.nav_list_item');
 
 const transition = document.querySelector('.transition'),
     transitionCol = transition.querySelector('.transition_column'),
@@ -16,86 +16,101 @@ const initTransition = () => {
 
 const show = () => {
     gsap.timeline({ defaults: { ease: 'expo.inOut' } })
-    .to(
-        transition,
-        {
-            duration: 0.2,
-            display: 'block',
-            autoAlpha: 1,
-        },
-        0
-    )
-    .to(
-        transitionCol,
-        {
-            duration: 1.25,
-            y: 0,
-            stagger: {
-                each: 0.045,
-                from: 'random',
-                grid: 'auto',
+        .to(
+            transition,
+            {
+                duration: 0.2,
+                display: 'block',
+                autoAlpha: 1,
             },
-        },
-        0.05
-    )
-    .to(
-        transitionColText,
-        {
-            duration: 1,
-            autoAlpha: 0,
-            y: 0,
-        },
-        0.5
-    )
-    .to(
-        transitionColText,
-        {
-            duration: 1,
-            autoAlpha: 0,
-            y: '101%',
-        },
-        2.75
-    )
-    .to(
-        transitionElementText,
-        {
-            duration: 1,
-            autoAlpha: 1,
-            stagger: {
-                each: 0.055,
-                from: 'random'
+            0
+        )
+        .to(
+            transitionCol,
+            {
+                duration: 1.25,
+                y: 0,
+                stagger: {
+                    each: 0.045,
+                    from: 'random',
+                    grid: 'auto',
+                },
             },
-            onComplete: () => {
-                hide();
+            0.05
+        )
+        .to(
+            transitionColText,
+            {
+                duration: 1,
+                autoAlpha: 0,
+                y: 0,
             },
-        },
-        0.75
-    )
+            0.5
+        )
+        .to(
+            transitionColText,
+            {
+                duration: 1,
+                autoAlpha: 0,
+                y: '101%',
+            },
+            2.75
+        )
+        .to(
+            transitionElementText,
+            {
+                duration: 1,
+                autoAlpha: 1,
+                stagger: {
+                    each: 0.055,
+                    from: 'random'
+                },
+                onComplete: () => {
+                    hide();
+                },
+            },
+            0.75
+        )
 };
 
 const hide = () => {
-    gsap.timeline({ defaults: { ease: 'expo.inOut' } }).to(
-        transitionElementText,
-        {
-            duration: 1,
-            autoAlpha: 0,
-            stagger: 0.055,
-        },
-        0
-    )
-    .to(transitionCol, {
-        duration: 1.25,
-        y: '101%',
-        stagger: {
-            each: 0.045,
-            from: 'random',
-            grid: 'auto',
-        },
-    },
-    0.75
-    .to()
-    )
+    gsap.timeline({ defaults: { ease: 'expo.inOut' } })
+        .to(
+            transitionElementText,
+            {
+                duration: 1,
+                autoAlpha: 0,
+                stagger: 0.055,
+            },
+            0
+        )
+        .to(
+            transitionCol,
+            {
+                duration: 1.25,
+                y: '101%',
+                stagger: {
+                    each: 0.045,
+                    from: 'random',
+                    grid: 'auto',
+                },
+            },
+            0.75
+        )
+        .to(
+            transition, {
+                duration: 0.2,
+                display: none,
+                autoAlpha: 0,
+                onComplete: initTransition,
+        });
 }
+
+navItem.forEach((el) => {
+    el.addEventListener('click', show);
+});
+
+
 
 window.onload = () => {
     initTransition();
